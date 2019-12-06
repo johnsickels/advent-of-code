e790ae08a28f04b33032f3c77433ae2066a06fa0
johnsickels <johnwsickels@gmail.com>
2019-12-06 13:57:31 -0500

    day one and day two, part one

---
+++ 1/day1.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream('./input.txt')
+});
+
+let totalFuel = 0;
+
+readInterface.on('line', function (mass) {
+
+    let addedFuel = (Math.floor(mass / 3) - 2);
+    let newFuel = 0;
+
+    totalFuel += addedFuel;
+    newFuel = Math.floor(addedFuel / 3) - 2
+
+    while (newFuel > 0){
+        totalFuel += newFuel
+        newFuel = Math.floor(newFuel / 3) - 2
+    }; 
+
+});
+
+setTimeout(() => console.log(totalFuel), 100);
+// correct answer 5239910
