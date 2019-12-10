aaa130619b0e45f783e8c0244df06c5eee449fe4
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-12-10 17:01:00 -0500

    day four, part one

---
+++ 4/part-one.js
@@ -0,0 +1,28 @@
+const min = 356261;
+const max = 846303;
+
+let passwords = [];
+
+passwordValidator = (a, b) => {
+  for (let i = a; i < b + 1; i++) {
+    iArr = ("" + i).split("").map(Number);
+    let incrementing = true;
+    let containsDups = false;
+
+    for (let j = 0; j < 5; j++) {
+      if (iArr[j] > iArr[j + 1]) {
+        incrementing = false;
+      }
+      if (iArr[j] === iArr[j + 1]) {
+        containsDups = true;
+      }
+    }
+
+    if (incrementing && containsDups) {
+      passwords.push(i);
+    }
+  }
+  console.log(`${passwords.length} different passwords meet the criteria`);
+};
+
+passwordValidator(min, max);
