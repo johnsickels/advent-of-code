958abbfff7dea42bd838405107b021c3e63edd8e
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-12-23 21:46:01 -0500

    day 5, part 2

--- 5/solution2.js
+++ 5/solution2.js
@@ -12,7 +12,6 @@ let param2;
 
 intcode = input => {
   while (i < len) {
-
     let opcode = arr[i];
 
     switch (opcode.toString().length) {
@@ -28,16 +27,14 @@ intcode = input => {
         opcode = Number(opcode.toString().split("")[2]);
         break;
       case 4:
-        param1 = (opcode.toString().split("")[1] === "1")
-          ? arr[i + 1]
-          : arr[arr[i + 1]];
+        param1 =
+          opcode.toString().split("")[1] === "1" ? arr[i + 1] : arr[arr[i + 1]];
         param2 = arr[i + 2];
         opcode = Number(opcode.toString().split("")[3]);
         break;
       default:
         console.log(`Error: Invalid OPCODE ${opcode}`);
     }
-
     switch (opcode) {
       case 1:
         arr[arr[i + 3]] = param1 + param2;
@@ -49,10 +46,10 @@ intcode = input => {
         break;
       case 3:
         arr[arr[i + 1]] = input;
-        i++;
+        i += 2;
         break;
       case 4:
-        console.log(`Position ${i} output ${param1}`)
+        console.log(`Position ${i} output ${param1}`);
         i += 2;
         break;
       case 5:
@@ -71,17 +68,17 @@ intcode = input => {
         break;
       case 7:
         if (param1 < param2) {
-          arr[i + 3] = 1;
+          arr[arr[i + 3]] = 1;
         } else {
-          arr[i + 3] = 0;
+          arr[arr[i + 3]] = 0;
         }
         i += 4;
         break;
       case 8:
         if (param1 === param2) {
-          arr[i + 3] = 1;
+          arr[arr[i + 3]] = 1;
         } else {
-          arr[i + 3] = 0;
+          arr[arr[i + 3]] = 0;
         }
         i += 4;
         break;
@@ -96,3 +93,5 @@ intcode = input => {
 
 intcode(5);
 // 7286649 was too low
+// 5204326 was too low
+// 15724522
