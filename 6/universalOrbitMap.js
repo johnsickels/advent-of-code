937ce5daa918d05a3675c9bbea909133873d3302
johnSickels <46609057+johnsickels@users.noreply.github.com>
2020-01-11 11:14:45 -0500

    Travis CI, part 3

--- 6/universalOrbitMap.js
+++ 6/universalOrbitMap.js
@@ -1,13 +1,10 @@
 const fs = require("fs");
-const arr = fs
-  .readFileSync("input.txt", "utf8")
-  .split('\n')
+const arr = fs.readFileSync("input.txt", "utf8").split("\n");
 
 let object;
-let orbits = []
+let orbits = [];
 
-  arr.forEach(orbit => {
-      object = orbit.split(')')
-      console.log(object);
-    
-  });
+arr.forEach(orbit => {
+  object = orbit.split(")");
+  console.log(object);
+});
