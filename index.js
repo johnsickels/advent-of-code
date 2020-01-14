2bd04b66c051a0f3449262b14577e5939cc8d07b
johnSickels <46609057+johnsickels@users.noreply.github.com>
2020-01-13 19:00:42 -0500

    mocha and chai

---
+++ index.js
@@ -0,0 +1,9 @@
+var multiply = function(x, y) {
+  if (typeof x !== "number" || typeof y !== "number") {
+    throw new Error("x or y is not a number.");
+  } else {
+    return x * y;
+  }
+};
+
+module.exports = multiply;
