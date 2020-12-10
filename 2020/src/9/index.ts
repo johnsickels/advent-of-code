c32a96d854cd62b1fe3008fe864a12f3e2ea1cc1
johnSickels <46609057+johnsickels@users.noreply.github.com>
2020-12-09 23:30:56 -0500

    day nine, part two

--- 2020/src/9/index.ts
+++ 2020/src/9/index.ts
@@ -3,7 +3,7 @@
 // const nums = inputToArray("9.txt").map((n: string) => parseInt(n))
 // const preamble = 25
 
-const isValid = (num: number, arr: number[]): boolean => {
+const isValid = (arr: number[], num: number): boolean => {
 
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
@@ -15,14 +15,30 @@ const isValid = (num: number, arr: number[]): boolean => {
     return false
 }
 
-export const findWeakness = (nums: number[], preamble: number): number => {
+export const hackXMAS = (nums: number[], preamble: number): { firstNumber: number, encryptionWeakness: number } => {
 
     for (let k = preamble; k < nums.length; k++) {
-        if (!isValid(nums[k], nums.slice(k - preamble, k))) {
-            return nums[k]
+        if (!isValid(nums.slice(k - preamble, k), nums[k])) {
+            const encryptionWeakness = findWeekness(nums.slice(0, k), nums[k])
+            return { firstNumber: nums[k], encryptionWeakness: encryptionWeakness }
         }
-
     }
 }
 
-// console.log(findWeakness(nums, preamble));
+const findWeekness = (nums: number[], target: number): number => {
+
+    for (let lower = 0; lower < nums.length; lower++) {
+        let upper = lower + 1
+        let sum;
+
+        do {
+            sum = nums.slice(lower, upper).reduce((acc, cur) => acc + cur)
+            if (sum === target) {
+                return Math.min(...nums.slice(lower, upper)) + Math.max(...nums.slice(lower, upper))
+            }
+            upper++
+        } while (sum <= target);
+    }
+}
+
+// console.log(hackXMAS(nums, preamble));
--- 2020/test/9.test.js
+++ 2020/test/9.test.js
@@ -1,37 +1,38 @@
 const { inputToArray } = require("../dist/utils");
-const { findWeakness } = require("../dist/9");
+const { hackXMAS } = require("../dist/9");
 
-test("Find the weakness in sample XMAS", () => {
-  expect(
-    findWeakness(
-      [
-        35,
-        20,
-        15,
-        25,
-        47,
-        40,
-        62,
-        55,
-        65,
-        95,
-        102,
-        117,
-        150,
-        182,
-        127,
-        219,
-        299,
-        277,
-        309,
-        576,
-      ],
-      5
-    )
-  ).toBe(127);
+const sample = [
+  35,
+  20,
+  15,
+  25,
+  47,
+  40,
+  62,
+  55,
+  65,
+  95,
+  102,
+  117,
+  150,
+  182,
+  127,
+  219,
+  299,
+  277,
+  309,
+  576,
+];
+
+test("Find the first invalid number in sample XMAS", () => {
+  expect(hackXMAS(sample, 5).firstNumber).toBe(127);
 });
 
 test("Find the weakness in actual XMAS", () => {
   const nums = inputToArray("9.txt").map((n) => parseInt(n));
-  expect(findWeakness(nums, 25)).toBe(177777905);
+  expect(hackXMAS(nums, 25).firstNumber).toBe(177777905);
+});
+
+test("Find the encryption weakness in sample XMAS", () => {
+  expect(hackXMAS(sample, 5).encryptionWeakness).toBe(62);
 });
