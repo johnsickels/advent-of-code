059a0d680501352a484cfaf308602c128310d624
John Sickels <johnwsickels@gmail.com>
2021-12-03 10:10:33 -0500

    day three, part two

--- 2021/src/3.ts
+++ 2021/src/3.ts
@@ -13,20 +13,58 @@ export const partOne = (array: string[]): number => {
     });
   });
 
-  let gamma = '';
-  let epsilon = '';
+  let gamma = "";
+  let epsilon = "";
 
   for (const key in obj) {
     if (Object.prototype.hasOwnProperty.call(obj, key)) {
       const element = obj[key];
-      const zeroMostCommon = element["0"] > element["1"]
+      const zeroMostCommon = element["0"] > element["1"];
       gamma += zeroMostCommon ? "0" : "1";
       epsilon += zeroMostCommon ? "1" : "0";
     }
   }
-    return parseInt(gamma, 2) * parseInt(epsilon, 2)
+  return parseInt(gamma, 2) * parseInt(epsilon, 2);
 };
 
-console.log(partOne(inputToArray("3.txt")));
+// console.log(partOne(inputToArray("3.txt")));
 
-// console.log(partOne(inputToArray("1.txt").map((e) => parseInt(e))));
+const getRating = (array: string[], ratingType: string, col = 0): number => {
+
+  if (array.length === 1) {
+    return parseInt(array[0], 2);
+  }
+
+  const zeroStarting = [];
+  const oneStarting = [];
+  
+  for (let j = 0; j < array.length; j++) {
+    const bit = array[j][col];
+    if (bit === "0") {
+      zeroStarting.push(array[j]);
+    } else {
+      oneStarting.push(array[j]);
+    }
+  }
+
+  if (ratingType === "oxygen") {
+    if (oneStarting.length >= zeroStarting.length) {
+      return getRating(oneStarting, ratingType, ++col);
+    } else {
+      return getRating(zeroStarting, ratingType, ++col);
+    }
+  }
+  if (ratingType === "CO2") {
+    if (oneStarting.length >= zeroStarting.length) {
+      return getRating(zeroStarting, ratingType, ++col);
+    } else {
+      return getRating(oneStarting, ratingType, ++col);
+    }
+  }
+};
+
+export const partTwo = (array: string[]): number => {
+  return getRating(array, "oxygen") * getRating(array, "CO2");
+};
+
+console.log(partTwo(inputToArray("3.txt")));
--- 2021/test/3.test.js
+++ 2021/test/3.test.js
@@ -1,22 +1,27 @@
-const { partOne } = require("../dist/3.js");
+const { partOne, partTwo } = require("../dist/3.js");
 const { inputToArray } = require("../dist/utils");
+
+const exampleReport = [
+  "00100",
+  "11110",
+  "10110",
+  "10111",
+  "10101",
+  "01111",
+  "00111",
+  "11100",
+  "10000",
+  "11001",
+  "00010",
+  "01010",
+];
 
 test("finds the power consumption of the submarine", () => {
   expect(partOne(inputToArray("3.txt"))).toBe(3309596);
-  expect(
-    partOne([
-      "00100",
-      "11110",
-      "10110",
-      "10111",
-      "10101",
-      "01111",
-      "00111",
-      "11100",
-      "10000",
-      "11001",
-      "00010",
-      "01010",
-    ])
-  ).toBe(198);
+  expect(partOne(exampleReport)).toBe(198);
+});
+
+test("finds the life support rating of the submarine", () => {
+  expect(partTwo(inputToArray("3.txt"))).toBe(2981085);
+  expect(partTwo(exampleReport)).toBe(230);
 });
