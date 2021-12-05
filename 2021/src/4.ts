3fcc84aef8c14f273ad59f5a7c6418876306fabe
John Sickels <johnwsickels@gmail.com>
2021-12-04 23:03:05 -0500

    day four part two

--- 2021/src/4.ts
+++ 2021/src/4.ts
@@ -3,14 +3,15 @@
 type Row = (number | string)[];
 type Board = Row[];
 
-export const partOne = (array: string[]): number => {
-  const draws = array[0].split(",").map((draw) => parseInt(draw));
-  const boards: Board[] = [];
+const getDraws = (rawArr: string[]) =>
+  rawArr[0].split(",").map((draw) => parseInt(draw));
 
-  // make 2D arrays
-  for (let i = 2; i < array.length; i += 6) {
+// make 2D arrays
+const getBoards = (rawArr: string[]) => {
+  const boards = [];
+  for (let i = 2; i < rawArr.length; i += 6) {
     boards.push(
-      array.slice(i, i + 5).map((row) => {
+      rawArr.slice(i, i + 5).map((row) => {
         return row
           .trim()
           .split(/\s+/)
@@ -18,19 +19,7 @@ export const partOne = (array: string[]): number => {
       })
     );
   }
-
-  // draw numbers
-  for (const draw of draws) {
-    // strike each draw with an x in every board
-    strikeBoards(boards, draw);
-
-    // check each board for a win
-    const winningBoard = checkForAWin(boards);
-    if (winningBoard && winningBoard.length) {
-      const sum = getSumOfRemaingNumbers(winningBoard);
-      return sum * draw;
-    }
-  }
+  return boards;
 };
 
 const strikeBoards = (boards: Board[], draw: number) => {
@@ -48,28 +37,40 @@ const strikeBoards = (boards: Board[], draw: number) => {
 
 const checkForAWin = (boards: Board[]) => {
   // check rows
-  for (const board of boards) {
+  for (let i = 0; i < boards.length; i++) {
+    const board = boards[i];
     for (const row of board) {
       // if no nums ( all exes )
       if (!row.some((num) => Number.isInteger(num))) {
-        return board;
+        return { board, i };
       }
     }
     // check columns
-    for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
       let exes = 0;
-      for (let j = 0; j < 5; j++) {
-        if (board[j][i] === "x") {
+      for (let k = 0; k < 5; k++) {
+        if (board[k][j] === "x") {
           exes++;
         }
-      }
-
-      // if entire column is exes
-      if (exes === 5) {
-        return board;
+        if (exes === 5) {
+          return { board, i };
+        }
       }
     }
   }
+};
+
+// i hate this global var
+const checkForWins = (boards: Board[], lastWonBoard?: Board): Board => {
+  // check each board for a win
+  const winningBoardObj = checkForAWin(boards);
+  // boards
+  if (winningBoardObj) {
+    lastWonBoard = boards.splice(winningBoardObj.i, 1)[0];
+    // keep cheking
+    return checkForWins(boards, lastWonBoard);
+  }
+  return lastWonBoard;
 };
 
 const getSumOfRemaingNumbers = (board: Board): number => {
@@ -84,4 +85,39 @@ const getSumOfRemaingNumbers = (board: Board): number => {
   return sum;
 };
 
-// console.log(partOne(inputToArray("4.txt")));
+export const partOne = (array: string[]): number => {
+  const draws = getDraws(array);
+  const boards: Board[] = getBoards(array);
+
+  // draw numbers
+  for (const draw of draws) {
+    // strike each draw with an x in every board
+    strikeBoards(boards, draw);
+
+    // check each board for a win
+    const winningBoardObj = checkForAWin(boards);
+    if (winningBoardObj) {
+      const sum = getSumOfRemaingNumbers(winningBoardObj.board);
+      return sum * draw;
+    }
+  }
+};
+
+export const partTwo = (array: string[]): number => {
+  const draws = getDraws(array);
+  const boards: Board[] = getBoards(array);
+  //   let wins = 0;
+  let lastWonBoard;
+
+  // draw numbers
+  for (const draw of draws) {
+    strikeBoards(boards, draw);
+
+    lastWonBoard = checkForWins(boards);
+    if (lastWonBoard && !boards.length) {
+      return getSumOfRemaingNumbers(lastWonBoard) * draw;
+    }
+  }
+};
+
+// console.log(partTwo(inputToArray("4.txt")));
--- 2021/test/4.test.js
+++ 2021/test/4.test.js
@@ -1,7 +1,12 @@
-const { partOne } = require("../dist/4.js");
+const { partOne, partTwo } = require("../dist/4.js");
 const { inputToArray } = require("../dist/utils");
 
 test("beats the squid", () => {
   expect(partOne(inputToArray("4.txt"))).toBe(23177);
   expect(partOne(inputToArray("4.test.txt"))).toBe(4512);
+});
+
+test("give squid the game", () => {
+  expect(partTwo(inputToArray("4.txt"))).toBe(6804);
+  expect(partTwo(inputToArray("4.test.txt"))).toBe(1924);
 });
