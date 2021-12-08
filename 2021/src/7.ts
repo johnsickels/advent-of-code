41870c008de31e4f2ab01e906a8e43f5ed819668
John Sickels <johnwsickels@gmail.com>
2021-12-07 20:50:22 -0500

    day seven part two

--- 2021/src/7.ts
+++ 2021/src/7.ts
@@ -1,14 +1,20 @@
-export const partOne = (array: number[]): number => {
+/**
+ * day seven parts one and two
+ * @param array current crab submarine positions
+ * @param mode economy for part one and sport for part two
+ * @returns minimum fuel spend
+ */
+export const main = (array: number[], mode = "economy"): number => {
   // get sum to find average position
   const sum = array.reduce((a, b) => a + b, 0);
 
   // get spend at middle point and +/- 1
   const averagePos = Math.round(sum / array.length) || 0;
-  const averageSpend = findFuelSpend(array, averagePos);
+  const averageSpend = findFuelTotalSpend(array, averagePos, mode);
   let upPos = averagePos + 1;
-  let upSpend = findFuelSpend(array, averagePos + 1);
+  let upSpend = findFuelTotalSpend(array, averagePos + 1, mode);
   let downPos = averagePos - 1;
-  let downSpend = findFuelSpend(array, averagePos - 1);
+  let downSpend = findFuelTotalSpend(array, averagePos - 1, mode);
 
   // determine direction
   let dir: string;
@@ -27,7 +33,7 @@ export const partOne = (array: number[]): number => {
   if (dir === "up") {
     while (minimumSpend > upSpend) {
       minimumSpend = upSpend;
-      upSpend = findFuelSpend(array, ++upPos);
+      upSpend = findFuelTotalSpend(array, ++upPos, mode);
     }
     return upSpend;
   }
@@ -35,12 +41,35 @@ export const partOne = (array: number[]): number => {
   if (dir === "down") {
     while (minimumSpend > downSpend) {
       minimumSpend = downSpend;
-      downSpend = findFuelSpend(array, --downPos);
+      downSpend = findFuelTotalSpend(array, --downPos, mode);
     }
     return minimumSpend;
   }
 };
 
-const findFuelSpend = (positions: number[], target: number) => {
-  return positions.reduce((prev, curr) => prev + Math.abs(curr - target), 0);
+// find fuel spend for all crab sumarines to reach a specified target
+const findFuelTotalSpend = (
+  positions: number[],
+  target: number,
+  mode: string
+) => {
+  return positions.reduce((prev, curr) => {
+    const fuelExpense =
+      mode === "sport"
+        ? findFuelSpend(Math.abs(curr - target))
+        : Math.abs(curr - target);
+    return prev + fuelExpense;
+  }, 0);
+};
+
+// find fuel spend in sport mode 5 + 4 + 3 + 2 + 1
+const findFuelSpend = (spacesToTravel: number): number => {
+  let spacesTraveled = 0;
+  let fuel = 0;
+  while (spacesToTravel) {
+    spacesTraveled++;
+    fuel += spacesTraveled;
+    spacesToTravel--;
+  }
+  return fuel;
 };
--- 2021/test/7.test.js
+++ 2021/test/7.test.js
@@ -1,12 +1,21 @@
-const { partOne } = require("../dist/7.js");
+const { main } = require("../dist/7.js");
 const { inputToArray } = require("../dist/utils");
 
 const sample = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
 
 test("finds minimum fuel spend to align all crab submarines", () => {
-  expect(partOne(sample)).toBe(37);
-  expect(
-    partOne(inputToArray("7.txt", 1, ",").map((num) => parseInt(num)))
-  ).toBe(336120);
+  expect(main(sample)).toBe(37);
+  expect(main(inputToArray("7.txt", 1, ",").map((num) => parseInt(num)))).toBe(
+    336120
+  );
 });
 
+test("finds minimum fuel spend to align all crab submarines in sport mode", () => {
+  expect(main(sample, "sport")).toBe(168);
+  expect(
+    main(
+      inputToArray("7.txt", 1, ",").map((num) => parseInt(num)),
+      "sport"
+    )
+  ).toBe(96864235);
+});
