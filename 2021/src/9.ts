0be2d8f9186a14076bbe197eceef42074ea30f90
John Sickels <johnwsickels@gmail.com>
2021-12-10 21:46:17 -0500

    day nine, part two

--- 2021/src/9.ts
+++ 2021/src/9.ts
@@ -1,47 +1,40 @@
 interface Point {
   riskLevel: number;
   visited: boolean;
-}
-
-interface LowPoint {
   x: number;
   y: number;
-  val: number;
 }
 
-const findLowestFromPoint = (
-  x: number,
-  y: number,
-  map: Point[][]
-): LowPoint => {
+const getCaveMap = (lines: string[]): Point[][] => {
+  // make a 2D array cave system
+  return lines.map((line, y) =>
+    line.split("").map((num, x) => {
+      // add one to value for risk level
+      return { riskLevel: parseInt(num) + 1, visited: false, x, y };
+    })
+  );
+};
+
+const findLowestFromPoint = (x: number, y: number, map: Point[][]): Point => {
   // don't retrace steps
   if (map[y][x].visited) return;
   map[y][x].visited = true;
 
   // gather comparison values
-  const val = map[y][x].riskLevel;
+  const riskLevel = map[y][x].riskLevel;
   const up = map[y + 1] && map[y + 1][x] && map[y + 1][x].riskLevel;
   const down = map[y - 1] && map[y - 1][x] && map[y - 1][x].riskLevel;
   const right = map[y][x + 1] && map[y][x + 1].riskLevel;
   const left = map[y][x - 1] && map[y][x - 1].riskLevel;
 
-  // check up and down
-  if (down < val) {
-    return findLowestFromPoint(x, y - 1, map);
-  }
-  if (up < val) {
-    return findLowestFromPoint(x, y + 1, map);
-  }
-  // check left and right
-  if (left < val) {
-    return findLowestFromPoint(x - 1, y, map);
-  }
-  if (right < val) {
-    return findLowestFromPoint(x + 1, y, map);
-  }
+  // check up, down, left and right
+  if (down < riskLevel) return findLowestFromPoint(x, y - 1, map);
+  if (up < riskLevel) return findLowestFromPoint(x, y + 1, map);
+  if (left < riskLevel) return findLowestFromPoint(x - 1, y, map);
+  if (right < riskLevel) return findLowestFromPoint(x + 1, y, map);
 
   // disregard wall points
-  const validPositions = [val, up, down, left, right].filter((pos) =>
+  const validPositions = [riskLevel, up, down, left, right].filter((pos) =>
     Number.isInteger(pos)
   );
 
@@ -51,28 +44,68 @@ const findLowestFromPoint = (
   }
 
   // this is a low point
-  return { x, y, val };
+  return { x, y, riskLevel, visited: true };
 };
 
-export const partOne = (input: string[]): number => {
-  // make a 2D array cave system
-  const map: Point[][] = input.map((line) =>
-    line.split("").map((num) => {
-      // add one to value for risk level
-      return { riskLevel: parseInt(num) + 1, visited: false };
-    })
-  );
-
+const findAllLowestPoints = (caveMap: Point[][]) => {
   const lowPoints = [];
 
-  for (let y = 0; y < map.length; y++) {
-    for (let x = 0; x < map[y].length; x++) {
-      const lowestPoint = findLowestFromPoint(x, y, map);
+  for (let y = 0; y < caveMap.length; y++) {
+    for (let x = 0; x < caveMap[y].length; x++) {
+      const lowestPoint = findLowestFromPoint(x, y, caveMap);
       if (lowestPoint) {
         lowPoints.push(lowestPoint);
       }
     }
   }
 
-  return lowPoints.map((lowPoint) => lowPoint.val).reduce((a, b) => a + b, 0);
+  return lowPoints;
+};
+
+// My first generator function
+// Thank you Harold
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function* getBasinPoints(x: number, y: number, map: Point[][]): any {
+  // current point value
+  const cur = map[y][x].riskLevel;
+
+  // get up down left right
+  const up = map[y + 1] && map[y + 1][x] && map[y + 1][x].riskLevel;
+  const down = map[y - 1] && map[y - 1][x] && map[y - 1][x].riskLevel;
+  const right = map[y][x + 1] && map[y][x + 1].riskLevel;
+  const left = map[y][x - 1] && map[y][x - 1].riskLevel;
+
+  // check each
+  // if larger, not null, and not peak, then
+  // delegate another generator
+  if (up > cur && up !== 10) yield* getBasinPoints(x, y + 1, map);
+  if (down > cur && down !== 10) yield* getBasinPoints(x, y - 1, map);
+  if (right > cur && right !== 10) yield* getBasinPoints(x + 1, y, map);
+  if (left > cur && left !== 10) yield* getBasinPoints(x - 1, y, map);
+
+  // if none match return basin coords as a string
+  yield `${y},${x}`;
+}
+
+export const partOne = (input: string[]): number => {
+  const caveMap = getCaveMap(input);
+  const lowPoints = findAllLowestPoints(caveMap);
+  return lowPoints
+    .map((lowPoint) => lowPoint.riskLevel)
+    .reduce((a, b) => a + b, 0);
+};
+
+export const partTwo = (input: string[]): number => {
+  const caveMap = getCaveMap(input);
+  const lowPoints = findAllLowestPoints(caveMap);
+
+  const basinSizes = lowPoints.map(
+    (point) =>
+      new Set(Array.from(getBasinPoints(point.x, point.y, caveMap))).size
+  );
+
+  return basinSizes
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((acc, cur) => acc * cur, 1);
 };
--- 2021/test/9.test.js
+++ 2021/test/9.test.js
@@ -1,7 +1,12 @@
-const { partOne } = require("../dist/9.js");
+const { partOne, partTwo } = require("../dist/9.js");
 const { inputToArray } = require("../dist/utils");
 
 test("Smoke Basin: Part One", () => {
   expect(partOne(inputToArray("9.test.txt"))).toBe(15);
   expect(partOne(inputToArray("9.txt"))).toBe(591);
+});
+
+test("Smoke Basin: Part Two", () => {
+  expect(partTwo(inputToArray("9.test.txt"))).toBe(1134);
+  expect(partTwo(inputToArray("9.txt"))).toBe(1113424);
 });
