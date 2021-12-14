a168b84e7e838a0d4391bed384aca389d9d9565b
John Sickels <johnwsickels@gmail.com>
2021-12-14 15:06:45 -0500

    day fourteen part two and refactor

--- 2021/src/14.ts
+++ 2021/src/14.ts
@@ -1,53 +1,120 @@
-type PairInsertionKey = Record<string, string>;
-type ElementCounter = Record<string, number>;
+type PairInsertionRules = Record<string, string[]>;
+type Counter = Record<string, number>;
 
-const getKey = (rules: string[]) => {
-  const pairInsertionKey: PairInsertionKey = {};
+/**
+ * Create an object whose values represent the two new pairs will replace the original pair
+ *
+ * Example:
+ * {  CH: [ 'CB', 'BH' ] }
+ *
+ * @param input Input array like CH -> B
+ * @returns
+ */
+const getPairInsertionRules = (input: string[]): PairInsertionRules => {
+  const pairInsertionRules: PairInsertionRules = {};
 
-  rules.forEach((pairInsertionRule) => {
+  input.forEach((pairInsertionRule) => {
     const [adjacents, insert] = pairInsertionRule.split(" -> ");
-    pairInsertionKey[adjacents] = insert;
+    pairInsertionRules[adjacents] = [
+      adjacents[0] + insert,
+      insert + adjacents[1],
+    ];
   });
 
-  return pairInsertionKey;
+  return pairInsertionRules;
 };
 
-const step = (polymerTemplate: string, key: PairInsertionKey) => {
-  for (let i = 0; i < polymerTemplate.length; i += 2) {
-    const pair = polymerTemplate[i] + polymerTemplate[i + 1];
-    const temp = polymerTemplate.split("");
-    temp.splice(i + 1, 0, key[pair]);
-    polymerTemplate = temp.join("");
+/**
+ * Create counter object of initial pairs from the input
+ *
+ * Example:
+ * { NN: 1, NC: 1, CB: 1 }
+ *
+ * @param polymerTemplate initial input like 'NNCB'
+ * @returns
+ */
+const getInitialPairs = (polymerTemplate: string): Counter => {
+  const pairs: Counter = {};
+
+  for (let i = 0; i < polymerTemplate.length - 1; i++) {
+    const currentPair = polymerTemplate[i] + polymerTemplate[i + 1];
+    pairs[currentPair] = pairs[currentPair] ? ++pairs[currentPair] : 1;
   }
-  return polymerTemplate;
+
+  return pairs;
 };
 
-const countElements = (polymerTemplate: string) => {
-  const elementCounter: ElementCounter = {};
-  polymerTemplate.split("").forEach((el) => {
-    elementCounter[el] = elementCounter[el] ? ++elementCounter[el] : 1;
+/**
+ * Create new counter object that substitues each pair with the two pairs after the insertion
+ *
+ * @param key pair insertion rules
+ * @param oldPairs pair conter object from last step
+ * @returns
+ */
+const step = (rules: PairInsertionRules, oldPairs: Counter): Counter => {
+  const newPairs: Counter = {};
+  Object.entries(oldPairs).forEach(([pair, frequency]) => {
+    rules[pair].forEach((newPair) => {
+      newPairs[newPair] = newPairs[newPair]
+        ? newPairs[newPair] + frequency
+        : frequency;
+    });
   });
-  return elementCounter;
+  return newPairs;
 };
 
-const getMostLeastDifference = (count: ElementCounter) => {
+/**
+ * Count elements from pairs
+ * @param pairs Counter object of pairs after all steps
+ * @returns Counter object of all elements
+ */
+const getElementCount = (pairs: Counter): Counter => {
+  const elCounter: Counter = {};
+
+  Object.entries(pairs).forEach(([pair, frequency]) => {
+    pair.split("").forEach((el) => {
+      // divide  by 2 because we count each pair once, and therefore character twice
+      elCounter[el] = elCounter[el]
+        ? elCounter[el] + frequency / 2
+        : frequency / 2;
+    });
+  });
+
+  // round up because the endcaps are only half points
+  Object.entries(elCounter).forEach(([pair, frequency]) => {
+    elCounter[pair] = Math.ceil(frequency);
+  });
+
+  return elCounter;
+};
+
+/**
+ * Sort the frequency of each element
+ * @param count Counter object of elements after all steps
+ * @returns Difference of most frequent element and least frequent element
+ */
+const getMostLeastDifference = (count: Counter): number => {
   const counts = Object.values(count);
   counts.sort((a, b) => b - a);
+
   return counts[0] - counts[counts.length - 1];
 };
 
-export const partOne = (input: string[]): number => {
-  let polymerTemplate = input[0];
+export const main = (input: string[], steps: number): number => {
+  const polymerTemplate = input[0];
   const pairInsertionRules = input[1].split("\n");
 
-  const pairInsertionKey = getKey(pairInsertionRules);
+  const rules = getPairInsertionRules(pairInsertionRules);
 
-  for (let i = 0; i <= 9; i++) {
-    polymerTemplate = step(polymerTemplate, pairInsertionKey);
+  let pairs = getInitialPairs(polymerTemplate);
+
+  for (let i = 0; i < steps; i++) {
+    pairs = step(rules, pairs);
   }
 
-  const count = countElements(polymerTemplate);
+  const elCounter = getElementCount(pairs);
 
-  const answer = getMostLeastDifference(count);
+  const answer = getMostLeastDifference(elCounter);
+
   return answer;
 };
--- 2021/test/14.test.js
+++ 2021/test/14.test.js
@@ -1,7 +1,12 @@
-const { partOne } = require("../dist/14.js");
+const { main } = require("../dist/14.js");
 const { inputToArray } = require("../dist/utils");
 
-test("Passage Pathing: Part One", () => {
-  expect(partOne(inputToArray("14.test.txt", 2))).toBe(1588);
-  expect(partOne(inputToArray("14.txt", 2))).toBe(2408);
+test("Extended Polymerization: Part One", () => {
+  expect(main(inputToArray("14.test.txt", 2), 10)).toBe(1588);
+  expect(main(inputToArray("14.txt", 2), 10)).toBe(2408);
+});
+
+test("Extended Polymerization: Part Two", () => {
+  expect(main(inputToArray("14.test.txt", 2), 40)).toBe(2188189693529);
+  expect(main(inputToArray("14.txt", 2), 40)).toBe(2651311098752);
 });
